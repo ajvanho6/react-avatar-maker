1cf9308041953a3fb2dbb6d14821d5e5ee9b92ee
Ivan Baresic <ivan.baresic@computerrock.com>
2018-04-25 17:29:19 +0200

    addremove code improvement

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -50,10 +50,9 @@ class Home extends React.Component {
     addOrRemove = (array, value) => {
         let index = array.indexOf(value);
         if (index === -1) {
-            array.push(value);
-        } else {
-            array.splice(index, 1);
+            return [...array, value];
         }
+            return array.splice(index, 1);
     }
 
     render() {
